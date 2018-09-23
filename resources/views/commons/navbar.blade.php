9a7e8c28b09fe5b1979056c95c077e381e89315a
thbcjp <contact@thbc.jp>
2018-09-23 18:20:15 +0000

    navbarを修正

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -17,7 +17,7 @@
                     <li class="dropdown caret">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li>{{ $user->name }}</li>
+                                
                                 <li role="separator" class="divider"></li>
                                 <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
                             </ul>
