7b41a0a20773ed69850859efc95f0b722644448e
thbcjp <contact@thbc.jp>
2018-09-23 18:32:15 +0000

    navbarに情報を受け渡し忘れていたので受け渡したつもtりなのだが、動かんかった

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -17,8 +17,6 @@
                     <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu caret">
-                                <?php $user = Auth::user(); ?>
-                                <li>{{ $user->name }}</li>
                                 <li role="separator" class="divider"></li>
                                 <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
                             </ul>
