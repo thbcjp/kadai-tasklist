b2e6b9556e10dbbcca17747d2534248066fe34d1
thbcjp <contact@thbc.jp>
2018-09-23 18:26:59 +0000

    navbarに情報を受け渡し忘れていたので受け渡した

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,10 +14,11 @@
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
                     <li><a href="#">Users</a></li>
-                    <li class="dropdown caret">
+                    <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                
+                            <ul class="dropdown-menu caret">
+                                <?php $user = Auth::user(); ?>
+                                <li>{{ $user->name }}</li>
                                 <li role="separator" class="divider"></li>
                                 <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
                             </ul>
