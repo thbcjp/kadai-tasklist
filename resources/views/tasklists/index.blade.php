fe97f274e4109b9a25d4383b82f59a3dba9b6a3d
thbcjp <contact@thbc.jp>
2018-09-24 18:12:17 +0000

    indexのみ修正3

--- resources/views/tasklists/index.blade.php
+++ resources/views/tasklists/index.blade.php
@@ -4,23 +4,11 @@
 
     @if (Auth::check())
     
-    @foreach ($tasks as $task)
+   
         <?php $user = Auth::user(); ?>
         {{ $user->name }}
         
-    <li class="media">
-        <div class="media-left">
-            <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
-        </div>
-        <div class="media-body">
-            <div>
-                {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">posted at {{ $task->created_at }}</span>
-            </div>
-            <div>
-                <p>{!! nl2br(e($task->content)) !!}</p>
-            </div>
-        </div>
-    </li>
+    
 
     <h1>タスク一覧</h1>
     
