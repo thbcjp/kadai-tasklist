a7dd1289cf44a461997b31d2a92f58bf6257eedc
thbcjp <contact@thbc.jp>
2018-09-24 17:49:08 +0000

    外部キー制約を追加

--- app/Task.php
+++ app/Task.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     // ホワイトリスト
-    protected $fillable = [ 'content', 'age', 10 ];
+    protected $fillable = [ 'content', 'status', 10 ];
     
     public function user(){
         return $this->belongsTo('App\User');
