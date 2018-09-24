8a874452cff34c2a5061372815fc88d52285f607
thbcjp <contact@thbc.jp>
2018-09-24 17:25:38 +0000

    Task.phpにbelongsToを追加 ユーザーごとのタスクリストにするため

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    // ホワイトリスト
+    protected $fillable = [ 'content', 'age', 10 ];
+    
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+    
 }
