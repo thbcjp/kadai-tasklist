3c48b8fa8fdc7e76e45143b8e50ede374303369a
thbcjp <contact@thbc.jp>
2018-09-27 13:57:07 +0000

    kansei

--- app/Task.php
+++ app/Task.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     // ホワイトリスト
-    protected $fillable = [ 'content', 'user_id', 'status', 10 ];
+    protected $fillable = [ 'content', 'user_id', 'status' ];
     
     public function user(){
-        return $this->belongsTo('App\User');
+        return $this->belongsTo(User::class);
     }
     
 }
