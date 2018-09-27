a8d57f2bed775102b3eec97ba01c5193dfde8309
thbcjp <contact@thbc.jp>
2018-09-27 13:40:38 +0000

    add_title＿tasksテーブルのの10を削除

--- database/migrations/2018_09_23_031030_add_title_to_tasks_table.php
+++ database/migrations/2018_09_23_031030_add_title_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddTitleToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status', 10);
+            $table->string('status');
         });
     }
 
@@ -26,7 +26,7 @@ class AddTitleToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('status', 10);
+            $table->dropColumn('status');
         });
     }
 }
